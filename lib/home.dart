7e75ed04b694bd47666601c4e8e4eaeedb146184
midnight23memories <87180323+midnight23memories@users.noreply.github.com>
2021-07-31 00:08:30 +0530

    Create home.dart

---
+++ lib/home.dart
@@ -0,0 +1,8 @@
+ final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController _emailController = new TextEditingController();
+  TextEditingController _passwordController = new TextEditingController();
+  TextEditingController _nameController = new TextEditingController();
+  String _email;
+  String _password;
+  String _displayName;
+  bool _obsecure = false;
