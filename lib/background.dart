eb65fbf6c605f71859fb277ce35a834b27b90605
midnight23memories <87180323+midnight23memories@users.noreply.github.com>
2021-08-01 18:53:13 +0530

    Update background.dart

--- lib/background.dart
+++ lib/background.dart
@@ -1,73 +1,21 @@
 import 'package:flutter/material.dart';
-@override
-  Widget build(BuildContext context) {
-  return Scaffold(
-        resizeToAvoidBottomPadding: false,
-        key: _scaffoldKey,
-        backgroundColor: Theme.of(context).primaryColor,
-        body: Column(
-         Widget logo() {
-      return Center(
-          child: Padding(
-        padding: EdgeInsets.only(top: 120),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: 240,
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                  child: Container(
-                child: Align(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle, color: Colors.white),
-                    width: 150,
-                    height: 150,
-                  ),
-                  alignment: Alignment.center,
-                ),
-                height: 154,
-              )),
-              Positioned(
-                child: Container(
-                    height: 154,
-                    width: MediaQuery.of(context).size.width,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        "GO",
-                        style: TextStyle(
-                          fontSize: 120,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    )),
-              ),
-              Positioned(
-                width: 60,
-                height: 60,
-                top: 140,
-                left: 260,
-                child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.white),
-                ),
-              ),
-              Positioned(
-                width: 30,
-                height: 30,
-                top: 200,
-                left: 230,
-                child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ));
-    }));
+
+class BottomWaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.moveTo(size.width, 0.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0.0, size.height);
+    path.lineTo(0.0, size.height + 5);
+    var secondControlPoint = Offset(size.width - (size.width / 6), size.height);
+    var secondEndPoint = Offset(size.width, 0.0);
+    path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
+        secondEndPoint.dx, secondEndPoint.dy);
+
+    return path;
   }
-  
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
